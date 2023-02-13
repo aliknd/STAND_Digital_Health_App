8023a7910d9664a37c2760dbe8e1abd26249e3ae
aliknd <a.kargaran.d@gmail.com>
2023-02-13 11:47:10 +0200

    First Version App Commit

---
+++ app/api/serverPoint.js
@@ -0,0 +1,3 @@
+const endpointURL = "http://192.168.0.100:9000/api";
+
+export default endpointURL;
---
+++ app/screens/DownloadScreen.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { Image, StyleSheet, Linking } from "react-native";
+import AppButton from "../components/AppButton";
+
+import Screen from "../components/Screen";
+import colors from "../config/colors";
+import AppText from "../components/AppText";
+
+function DownloadScreen(props) {
+  return (
+    <Screen style={styles.container}>
+      <Image style={styles.logo} source={require("../assets/uhmlogo.jpg")} />
+      <AppText style={styles.explanation}>
+        There is no right or wrong way to feel during unprecedented times. Allow
+        yourself to experience the emotions, but remind yourself that this is
+        not forever. University of Hawai'i at Manoa has prepared a comprehensive
+        guideline to help the patients dealing with mental issues. You can now
+        download it from the link bellow!
+      </AppText>
+      <AppButton
+        title="Download"
+        onPress={() =>
+          Linking.openURL(
+            "https://research.hawaii.edu/orc/wp-content/uploads/sites/7/2021/12/mental-health-and-suicide-crisis.pdf"
+          )
+        }
+      />
+    </Screen>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+    backgroundColor: colors.lightGreen,
+  },
+  logo: {
+    width: "100%",
+    height: 130,
+    alignSelf: "center",
+    marginTop: 5,
+    marginBottom: 15,
+    borderRadius: 20,
+  },
+  explanation: {
+    backgroundColor: colors.darkGreen,
+    padding: 15,
+    marginBottom: 10,
+    borderRadius: 20,
+    textAlign: "justify",
+  },
+});
+
+export default DownloadScreen;
