1aba8c74f2ce659122f18988dd0caf72ab731d5a
zhixinli <98064045+zhixinliuh@users.noreply.github.com>
2023-11-22 12:15:31 -1000

    start game screen and score screen

--- app/api/serverPoint.js
+++ app/api/serverPoint.js
@@ -1,3 +1,3 @@
-const endpointURL = "http:///168.105.245.96:9000/api";
+const endpointURL = "http:///168.105.229.214:9000/api";
 
 export default endpointURL;
--- app/screens/StroopScreen.js
+++ app/screens/StroopScreen.js
@@ -2,13 +2,15 @@ import React, { useState, useEffect } from "react";
 import { StyleSheet, View, Text, TouchableOpacity, Alert } from "react-native";
 import { Camera, CameraType } from 'expo-camera';
 
-const colors = ["red", "blue", "green", "yellow"];
+const colors = ["red", "blue", "green", "yellow", "purple", "orange", "pink"];
 
 const getRandomColor = () => colors[Math.floor(Math.random() * colors.length)];
 const getShuffledColorsWithAnswer = (answer) => {
   const otherColors = colors.filter(c => c !== answer);
-  const allColors = [answer, ...otherColors];
-  return allColors.sort(() => 0.5 - Math.random()).slice(0, 4);
+  const shuffledOtherColors = otherColors.sort(() => 0.5 - Math.random());
+  const selectedColors = shuffledOtherColors.slice(0, 3);
+  const allOptions = [answer, ...selectedColors].sort(() => 0.5 - Math.random());
+  return allOptions;
 };
 
 function GameScreen1() {
@@ -20,45 +22,60 @@ function GameScreen1() {
   const [timerWidth, setTimerWidth] = useState(100);
   const [decisionTime, setDecisionTime] = useState(5);
   const [hasPermission, setHasPermission] = useState(null);
+  const [preGameCountdown, setPreGameCountdown] = useState(3);
+  const [isGameStarted, setIsGameStarted] = useState(false);
+  const [isGameOver, setIsGameOver] = useState(false);
 
   useEffect(() => {
-    // Camera permission
     (async () => {
       const { status } = await Camera.requestCameraPermissionsAsync();
       setHasPermission(status === 'granted');
     })();
-    const maxTime = 30;
 
-    const countdown = setInterval(() => {
-      setTimer(prevTimer => {
-        if (prevTimer <= 1) {
-          clearInterval(countdown);
-          Alert.alert("Time's up!", `Your score is: ${score}`);
-          clearInterval(decisionCountdown); // stop the decision countdown
-          return 0;
-        } else {
-          setTimerWidth((prevTimer - 1) / maxTime * 100);
-        }
-        return prevTimer - 1;
-      });
-    }, 1000);
+    let preGameTimer = null;
+    if (!isGameStarted && preGameCountdown > 0) {
+      preGameTimer = setTimeout(() => {
+        setPreGameCountdown(preGameCountdown - 1);
+      }, 1000);
+    } else if (!isGameStarted) {
+      setIsGameStarted(true);
+    }
 
-    const decisionCountdown = setInterval(() => {
-      setDecisionTime(prevDecisionTime => {
-        if (prevDecisionTime <= 1) {
-          clearInterval(decisionCountdown);
-          handlePress('');
-          return 5;
-        }
-        return prevDecisionTime - 1;
-      });
-    }, 1000);
+    let countdown = null;
+    let decisionCountdown = null;
+    if (isGameStarted && !isGameOver) {
+      countdown = setInterval(() => {
+        setTimer(prevTimer => {
+          if (prevTimer <= 1) {
+            clearInterval(countdown);
+            clearInterval(decisionCountdown);
+            setIsGameOver(true);
+            return 0;
+          } else {
+            setTimerWidth((prevTimer - 1) / 30 * 100);
+          }
+          return prevTimer - 1;
+        });
+      }, 1000);
+
+      decisionCountdown = setInterval(() => {
+        setDecisionTime(prevDecisionTime => {
+          if (prevDecisionTime <= 1) {
+            clearInterval(decisionCountdown);
+            handlePress('');
+            return 5;
+          }
+          return prevDecisionTime - 1;
+        });
+      }, 1000);
+    }
 
     return () => {
-      clearInterval(countdown);
-      clearInterval(decisionCountdown);
+      if (countdown) clearInterval(countdown);
+      if (decisionCountdown) clearInterval(decisionCountdown);
+      if (preGameTimer) clearTimeout(preGameTimer);
     };
-  }, [score]);
+  }, [score, preGameCountdown, isGameStarted, isGameOver]);
 
   const handlePress = (selectedColor) => {
     if (selectedColor === displayColor) {
@@ -66,13 +83,22 @@ function GameScreen1() {
     } else {
       setScore(score - 1);
     }
-
     setDecisionTime(5);
     const newTextColor = getRandomColor();
     const newDisplayColor = getRandomColor();
     setTextColor(newTextColor);
     setDisplayColor(newDisplayColor);
     setOptions(getShuffledColorsWithAnswer(newDisplayColor));
+  };
+
+  const restartGame = () => {
+    setIsGameOver(false);
+    setIsGameStarted(false);
+    setPreGameCountdown(3);
+    setScore(0);
+    setTimer(30);
+    setTimerWidth(100);
+    setDecisionTime(5);
   };
 
   if (hasPermission === null) {
@@ -82,36 +108,54 @@ function GameScreen1() {
     return <Text>No access to camera</Text>;
   }
 
-  return (
-    <View style={styles.container}>
-      <Camera
-        style={styles.preview}
-        type={Camera.Constants.Type.front}
-        ratio={"16:9"}
-      >
-        <View style={styles.overlay}>
-          {/* ...Your game UI here... */}
-          <View style={styles.timerBarContainer}>
-            <View style={[styles.timerBar, { width: `${timerWidth}%` }]}></View>
-          </View>
-          <View style={styles.scoreContainer}>
-            <Text style={styles.scoreText}>Score: {score}</Text>
-            <Text style={styles.timerText}>Time: {timer}s</Text>
-            <Text style={styles.decisionTimerText}>Answer in: {decisionTime}s</Text>
-          </View>
-          <View style={styles.centered}>
-            <Text style={[styles.text, { color: displayColor }]}>{textColor}</Text>
-          </View>
-          <View style={styles.optionsContainer}>
-            {options.map((color) => (
-              <TouchableOpacity key={color} style={[styles.button]} onPress={() => handlePress(color)}>
-                <Text style={styles.buttonText}>{color}</Text>
-              </TouchableOpacity>
-            ))}
-          </View>
+  if (!isGameStarted) {
+    return (
+        <View style={styles.centered}>
+          <Text style={styles.countdownText}>Game starts in: {preGameCountdown}</Text>
         </View>
-      </Camera>
-    </View>
+    );
+  }
+
+  if (isGameOver) {
+    return (
+        <View style={styles.centered}>
+          <Text style={styles.finalScoreText}>Final Score: {score}</Text>
+          <TouchableOpacity style={styles.restartButton} onPress={restartGame}>
+            <Text style={styles.restartButtonText}>Restart Game</Text>
+          </TouchableOpacity>
+        </View>
+    );
+  }
+
+  return (
+      <View style={styles.container}>
+        <Camera
+            style={styles.preview}
+            type={CameraType.front}
+            ratio={"16:9"}
+        >
+          <View style={styles.overlay}>
+            <View style={styles.timerBarContainer}>
+              <View style={[styles.timerBar, { width: `${timerWidth}%` }]}></View>
+            </View>
+            <View style={styles.scoreContainer}>
+              <Text style={styles.scoreText}>Score: {score}</Text>
+              <Text style={styles.timerText}>Time: {timer}s</Text>
+              <Text style={styles.decisionTimerText}>Answer in: {decisionTime}s</Text>
+            </View>
+            <View style={styles.centered}>
+              <Text style={[styles.text, { color: displayColor }]}>{textColor}</Text>
+            </View>
+            <View style={styles.optionsContainer}>
+              {options.map((color) => (
+                  <TouchableOpacity key={color} style={[styles.button]} onPress={() => handlePress(color)}>
+                    <Text style={styles.buttonText}>{color}</Text>
+                  </TouchableOpacity>
+              ))}
+            </View>
+          </View>
+        </Camera>
+      </View>
   );
 }
 
@@ -139,6 +183,27 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  countdownText: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#2c3e50',
+  },
+  finalScoreText: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  restartButton: {
+    backgroundColor: 'blue',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 5,
+  },
+  restartButtonText: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold',
   },
   text: {
     fontSize: 40,
